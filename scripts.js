16436d350ed28e29b9bdaff34d498a0e2840a036
m-jerwan <36174379+m-jerwan@users.noreply.github.com>
2018-03-26 12:13:06 +0100

    ---connected random quotes to js script and link jpg and bcg together. --loop and functioned it to get as DRY as possible

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
 
+//============  SWAPPER:
+var i=1
+while (i<6) {
+  var randomNo =+Math.floor((Math.random() * 5) + 1); //----[PROBLEM: PICTURES MIGHT APPEAR TWICE:]
+  document.getElementById('imgq'+i).src = "img/img"+randomNo+".jpg";   //changing img
+  document.getElementById('quote'+i).style.backgroundImage = "url(img/bcg"+randomNo+".jpg)";  //changing bcg
+  i=i+1;
+}
